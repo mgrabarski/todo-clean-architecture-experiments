407a28d14499bbd093381d9fbd32e23053644d7f
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-10-27 14:55:37 +0200

    Added get function to list repository

--- domain/src/main/java/blog/mateuszgrabarski/todo/domain/repositories/TodoListRepository.kt
+++ domain/src/main/java/blog/mateuszgrabarski/todo/domain/repositories/TodoListRepository.kt
@@ -1,7 +1,9 @@
 package blog.mateuszgrabarski.todo.domain.repositories
 
+import blog.mateuszgrabarski.todo.domain.models.Id
 import blog.mateuszgrabarski.todo.domain.models.TodoList
 
 interface TodoListRepository {
     suspend fun saveList(newList: TodoList)
+    suspend fun getById(id: Id): TodoList?
 }
