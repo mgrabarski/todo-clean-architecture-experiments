9a55507ea60f4a6fb70c986f6826d042752db6f4
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-10-28 13:03:37 +0200

    Added empty implementation for todo list repository

---
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/TodoListRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package blog.mateuszgrabarski.todo.data.store
+
+import blog.mateuszgrabarski.todo.data.store.cache.TodoListCacheDataSource
+import blog.mateuszgrabarski.todo.data.store.network.TodoListRemoteDataSource
+import blog.mateuszgrabarski.todo.domain.models.Id
+import blog.mateuszgrabarski.todo.domain.models.TodoList
+import blog.mateuszgrabarski.todo.domain.repositories.TodoListRepository
+
+class TodoListRepositoryImpl(
+    private val cacheDataSource: TodoListCacheDataSource,
+    private val remoteDataSource: TodoListRemoteDataSource
+) : TodoListRepository {
+
+    override suspend fun saveList(newList: TodoList) {
+        cacheDataSource.insert(newList)
+        remoteDataSource.insert(newList)
+    }
+
+    override suspend fun getById(id: Id): TodoList? {
+        TODO("Not yet implemented")
+    }
+}
