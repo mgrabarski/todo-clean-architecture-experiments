5a8a10d05df486521552168d3ab5e71a01dca37a
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-05 12:40:07 +0100

    Fix for deleting todo list

--- domain/src/main/java/blog/mateuszgrabarski/todo/domain/usecases/lists/impl/DeleteTodoListImpl.kt
+++ domain/src/main/java/blog/mateuszgrabarski/todo/domain/usecases/lists/impl/DeleteTodoListImpl.kt
@@ -2,7 +2,8 @@ package blog.mateuszgrabarski.todo.domain.usecases.lists.impl
 
 import blog.mateuszgrabarski.todo.domain.repositories.TodoListRepository
 import blog.mateuszgrabarski.todo.domain.usecases.lists.DeleteTodoList
-import blog.mateuszgrabarski.todo.domain.usecases.lists.UpdateTodoList
+import blog.mateuszgrabarski.todo.domain.usecases.lists.DeleteTodoList.Arguments
+import blog.mateuszgrabarski.todo.domain.usecases.lists.DeleteTodoList.Companion.ERROR_LIST_NOT_FOUND
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Failure
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Result
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Success
@@ -13,11 +14,11 @@ class DeleteTodoListImpl(
     private val repository: TodoListRepository
 ) : DeleteTodoList {
 
-    override suspend fun execute(argument: DeleteTodoList.Arguments): Flow<Result<Boolean>> = flow {
+    override suspend fun execute(argument: Arguments): Flow<Result<Boolean>> = flow {
         val list = repository.getById(argument.listId)
 
         if (list == null) {
-            Failure(UpdateTodoList.ERROR_LIST_NOT_FOUND)
+            emit(Failure(ERROR_LIST_NOT_FOUND))
             return@flow
         }
 
--- domain/src/test/java/blog/mateuszgrabarski/todo/domain/usecases/lists/impl/DeleteTodoListImplTest.kt
+++ domain/src/test/java/blog/mateuszgrabarski/todo/domain/usecases/lists/impl/DeleteTodoListImplTest.kt
@@ -1,5 +1,6 @@
 package blog.mateuszgrabarski.todo.domain.usecases.lists.impl
 
+import app.cash.turbine.test
 import blog.mateuszgrabarski.todo.domain.fakes.FakeToDoListRepository
 import blog.mateuszgrabarski.todo.domain.fakes.data.anyTodoList
 import blog.mateuszgrabarski.todo.domain.models.Id
@@ -7,23 +8,30 @@ import blog.mateuszgrabarski.todo.domain.usecases.lists.DeleteTodoList.Arguments
 import blog.mateuszgrabarski.todo.domain.usecases.lists.DeleteTodoList.Companion.ERROR_LIST_NOT_FOUND
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Failure
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Success
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class DeleteTodoListImplTest {
 
-    private val repository = FakeToDoListRepository()
-    private val sut = DeleteTodoListImpl(repository)
+    private lateinit var repository: FakeToDoListRepository
+    private lateinit var sut: DeleteTodoListImpl
+
+    @BeforeEach
+    internal fun setUp() {
+        repository = FakeToDoListRepository()
+        sut = DeleteTodoListImpl(repository)
+    }
 
     @Test
     internal fun `Emits error when list is not available in repo`() = runBlocking {
         sut.execute(
             Arguments(ANY_ID)
-        ).collect {
-            val result = (it as Failure).message
-            assertEquals(ERROR_LIST_NOT_FOUND, result)
+        ).test {
+            val result = awaitItem() as Failure
+            assertEquals(ERROR_LIST_NOT_FOUND, result.message)
+            awaitComplete()
         }
     }
 
@@ -32,10 +40,11 @@ internal class DeleteTodoListImplTest {
         repository.saveList(anyTodoList(ANY_ID))
         sut.execute(
             Arguments(ANY_ID)
-        ).collect {
-            val result = (it as Success<Boolean>).data
+        ).test {
+            val result = (awaitItem() as Success<Boolean>).data
             assertTrue(result)
             assertNull(repository.getById(ANY_ID))
+            awaitComplete()
         }
     }
 
