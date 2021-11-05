5e6deaf2d54af5a364ecef127698ab4c8b82c2cf
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-05 11:35:35 +0100

    Failure to add new todo when no list found

--- domain/src/main/java/blog/mateuszgrabarski/todo/domain/usecases/todo/impl/AddTodoToListImpl.kt
+++ domain/src/main/java/blog/mateuszgrabarski/todo/domain/usecases/todo/impl/AddTodoToListImpl.kt
@@ -6,6 +6,8 @@ import blog.mateuszgrabarski.todo.domain.repositories.TodoListRepository
 import blog.mateuszgrabarski.todo.domain.repositories.TodoRepository
 import blog.mateuszgrabarski.todo.domain.usecases.todo.AddTodoToList
 import blog.mateuszgrabarski.todo.domain.usecases.todo.AddTodoToList.Arguments
+import blog.mateuszgrabarski.todo.domain.usecases.todo.AddTodoToList.Companion.ERROR_LIST_NOT_FOUND
+import blog.mateuszgrabarski.todo.domain.usecases.utils.Failure
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Result
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Success
 import kotlinx.coroutines.flow.Flow
@@ -20,9 +22,14 @@ class AddTodoToListImpl(
     override suspend fun execute(argument: Arguments): Flow<Result<Todo>> = flow {
         val list = listRepository.getById(argument.listId)
 
+        if (list == null) {
+            emit(Failure(ERROR_LIST_NOT_FOUND))
+            return@flow
+        }
+
         val todo = todoFactory.create(argument.description, argument.listId)
 
-        list!!.addTodo(todo)
+        list.addTodo(todo)
         todoRepository.saveTodo(todo)
         emit(Success(todo))
     }
--- domain/src/test/java/blog/mateuszgrabarski/todo/domain/usecases/todo/impl/AddTodoToListImplTest.kt
+++ domain/src/test/java/blog/mateuszgrabarski/todo/domain/usecases/todo/impl/AddTodoToListImplTest.kt
@@ -7,18 +7,29 @@ import blog.mateuszgrabarski.todo.domain.fakes.data.anyTodoList
 import blog.mateuszgrabarski.todo.domain.models.Id
 import blog.mateuszgrabarski.todo.domain.models.Todo
 import blog.mateuszgrabarski.todo.domain.usecases.todo.AddTodoToList.Arguments
+import blog.mateuszgrabarski.todo.domain.usecases.todo.AddTodoToList.Companion.ERROR_LIST_NOT_FOUND
+import blog.mateuszgrabarski.todo.domain.usecases.utils.Failure
 import blog.mateuszgrabarski.todo.domain.usecases.utils.Success
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class AddTodoToListImplTest {
 
-    private val todoFactory = TodoFactory()
-    private val listRepository = FakeToDoListRepository()
-    private val todoRepository = FakeTodoRepository()
-    private val sut = AddTodoToListImpl(todoFactory, listRepository, todoRepository)
+    private lateinit var todoFactory: TodoFactory
+    private lateinit var listRepository: FakeToDoListRepository
+    private lateinit var todoRepository: FakeTodoRepository
+    private lateinit var sut: AddTodoToListImpl
+
+    @BeforeEach
+    internal fun setUp() {
+        todoFactory = TodoFactory()
+        listRepository = FakeToDoListRepository()
+        todoRepository = FakeTodoRepository()
+        sut = AddTodoToListImpl(todoFactory, listRepository, todoRepository)
+    }
 
     @Test
     internal fun `Creates todo in list`() = runBlocking {
@@ -32,6 +43,19 @@ internal class AddTodoToListImplTest {
         ).collect {
             val result = it as Success<Todo>
             assertEquals(todoRepository.getById(result.data.id), result.data)
+        }
+    }
+
+    @Test
+    internal fun `Failure when todo list not found`() = runBlocking {
+        sut.execute(
+            Arguments(
+                description = VALID_DESCRIPTION,
+                listId = ANY_LIST_ID
+            )
+        ).collect {
+            val result = it as Failure
+            assertEquals(ERROR_LIST_NOT_FOUND, result.message)
         }
     }
 
