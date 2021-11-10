aa66691fdcfd97188d774b19ac8b6993ac4c0e06
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-10 12:22:51 +0100

    Added DI config to data module

--- app/src/main/java/blog/mateuszgrabarski/todo/application/di/AppComponents.kt
+++ app/src/main/java/blog/mateuszgrabarski/todo/application/di/AppComponents.kt
@@ -1,7 +1,9 @@
 package blog.mateuszgrabarski.todo.application.di
 
+import blog.mateuszgrabarski.todo.data.di.dataModule
 import blog.mateuszgrabarski.todo.domain.di.domainModule
 
 val components = listOf(
-    domainModule
+    domainModule,
+    dataModule
 )
---
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/di/Components.kt
@@ -0,0 +1,7 @@
+package blog.mateuszgrabarski.todo.data.di
+
+import org.koin.dsl.module
+
+val dataModule = module {
+
+}
