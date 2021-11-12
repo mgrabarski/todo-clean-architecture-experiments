b2ad48f1f21bf962207b8c66e4fb6cd6a4387901
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-12 13:16:01 +0100

    Added network errors

---
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/network/NetworkErrors.kt
@@ -0,0 +1,6 @@
+package blog.mateuszgrabarski.todo.data.store.network
+
+object NetworkErrors {
+    const val NETWORK_ERROR_UNKNOWN = "Unknown network error"
+    const val NETWORK_ERROR_TIMEOUT = "Network timeout"
+}
