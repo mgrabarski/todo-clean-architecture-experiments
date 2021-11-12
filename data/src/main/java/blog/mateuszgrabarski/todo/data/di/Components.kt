427454fdfea40654c9c5436059aec433ac31d95b
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-12 09:11:09 +0100

    Di update

--- data/src/main/java/blog/mateuszgrabarski/todo/data/di/Components.kt
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/di/Components.kt
@@ -1,7 +1,8 @@
 package blog.mateuszgrabarski.todo.data.di
 
+import blog.mateuszgrabarski.todo.data.model.mappers.specific.DomainEntityMapper
 import org.koin.dsl.module
 
 val dataModule = module {
-
+    factory { DomainEntityMapper() }
 }
