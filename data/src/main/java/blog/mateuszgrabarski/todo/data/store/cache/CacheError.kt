d77e34bab099a30345e28b9f44f765ee06dfb497
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-12 13:11:09 +0100

    Added cache errors

---
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/cache/CacheError.kt
@@ -0,0 +1,6 @@
+package blog.mateuszgrabarski.todo.data.store.cache
+
+object CacheErrors {
+    const val CACHE_ERROR_UNKNOWN = "Unknown cache error"
+    const val CACHE_ERROR_TIMEOUT = "Cache timeout"
+}
