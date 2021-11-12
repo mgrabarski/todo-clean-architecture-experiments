4ab978e3fd4133fa0faa102f42a17c8ce20448b9
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-12 09:10:58 +0100

    Added entity and domain models mapper

---
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/model/mappers/specific/DomainEntityMapper.kt
@@ -0,0 +1,26 @@
+package blog.mateuszgrabarski.todo.data.model.mappers.specific
+
+import blog.mateuszgrabarski.todo.data.model.entities.TodoListEntity
+import blog.mateuszgrabarski.todo.data.model.mappers.DomainModelMapper
+import blog.mateuszgrabarski.todo.domain.models.TodoList
+
+class DomainEntityMapper : DomainModelMapper<TodoListEntity, TodoList> {
+
+    override fun mapFromEntity(entity: TodoListEntity): TodoList =
+        TodoList(
+            id = entity.id,
+            name = entity.name,
+            description = entity.description,
+            createDate = entity.createDate,
+            modificationDateTime = entity.modificationDateTime
+        )
+
+    override fun mapToEntity(domainModel: TodoList): TodoListEntity =
+        TodoListEntity(
+            id = domainModel.id,
+            name = domainModel.name,
+            description = domainModel.description,
+            createDate = domainModel.createDate,
+            modificationDateTime = domainModel.modificationDateTime
+        )
+}
