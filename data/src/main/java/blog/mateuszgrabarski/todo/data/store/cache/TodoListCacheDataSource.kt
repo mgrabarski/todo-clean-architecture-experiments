4a1e28982b6f5ec0e043cc56c060309a398742de
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-12 11:33:37 +0100

    Changed operation objects in data sources

--- data/src/main/java/blog/mateuszgrabarski/todo/data/store/cache/TodoListCacheDataSource.kt
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/cache/TodoListCacheDataSource.kt
@@ -1,7 +1,11 @@
 package blog.mateuszgrabarski.todo.data.store.cache
 
-import blog.mateuszgrabarski.todo.domain.models.TodoList
+import blog.mateuszgrabarski.todo.data.model.entities.TodoListEntity
+import blog.mateuszgrabarski.todo.domain.models.Id
 
 interface TodoListCacheDataSource {
-    suspend fun insert(newList: TodoList)
+    suspend fun insert(newList: TodoListEntity)
+    suspend fun get(id: Id): TodoListEntity?
+    suspend fun update(list: TodoListEntity)
+    suspend fun delete(id: Id)
 }
--- data/src/main/java/blog/mateuszgrabarski/todo/data/store/network/TodoListRemoteDataSource.kt
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/network/TodoListRemoteDataSource.kt
@@ -1,7 +1,11 @@
 package blog.mateuszgrabarski.todo.data.store.network
 
-import blog.mateuszgrabarski.todo.domain.models.TodoList
+import blog.mateuszgrabarski.todo.data.model.entities.TodoListEntity
+import blog.mateuszgrabarski.todo.domain.models.Id
 
 interface TodoListRemoteDataSource {
-    suspend fun insert(newList: TodoList)
+    suspend fun insert(newList: TodoListEntity)
+    suspend fun get(id: Id): TodoListEntity?
+    suspend fun update(list: TodoListEntity)
+    suspend fun delete(id: Id)
 }
