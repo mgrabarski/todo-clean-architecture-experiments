939761c2d937a8388b763923f5cbcf0049244718
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-11-12 11:35:33 +0100

    Updated fakes

--- domain/src/test/java/blog/mateuszgrabarski/todo/domain/fakes/FakeToDoListRepository.kt
+++ domain/src/test/java/blog/mateuszgrabarski/todo/domain/fakes/FakeToDoListRepository.kt
@@ -2,24 +2,28 @@ package blog.mateuszgrabarski.todo.domain.fakes
 
 import blog.mateuszgrabarski.todo.domain.models.Id
 import blog.mateuszgrabarski.todo.domain.models.TodoList
+import blog.mateuszgrabarski.todo.domain.repositories.Success
 import blog.mateuszgrabarski.todo.domain.repositories.TodoListRepository
 
 class FakeToDoListRepository : TodoListRepository {
 
     private val lists = mutableMapOf<Id, TodoList>()
 
-    override suspend fun saveList(newList: TodoList) {
+    override suspend fun saveList(newList: TodoList): Success {
         lists[newList.id] = newList
+        return true
     }
 
     override suspend fun getById(id: Id): TodoList? = lists[id]
 
-    override suspend fun update(list: TodoList) {
+    override suspend fun update(list: TodoList): Success {
         lists[list.id] = list
+        return true
     }
 
-    override suspend fun delete(id: Id) {
+    override suspend fun delete(id: Id): Success {
         lists.remove(id)
+        return true
     }
 
     override suspend fun getAllLists(): List<TodoList> = lists.values.toList()
--- domain/src/test/java/blog/mateuszgrabarski/todo/domain/fakes/FakeTodoRepository.kt
+++ domain/src/test/java/blog/mateuszgrabarski/todo/domain/fakes/FakeTodoRepository.kt
@@ -2,6 +2,7 @@ package blog.mateuszgrabarski.todo.domain.fakes
 
 import blog.mateuszgrabarski.todo.domain.models.Id
 import blog.mateuszgrabarski.todo.domain.models.Todo
+import blog.mateuszgrabarski.todo.domain.repositories.Success
 import blog.mateuszgrabarski.todo.domain.repositories.TodoRepository
 
 class FakeTodoRepository : TodoRepository {
@@ -9,27 +10,31 @@ class FakeTodoRepository : TodoRepository {
     private val notCompleted = mutableMapOf<Id, Todo>()
     private val completed = mutableMapOf<Id, Todo>()
 
-    override suspend fun saveTodo(todo: Todo) {
+    override suspend fun saveTodo(todo: Todo): Success {
         notCompleted[todo.id] = todo
+        return true
     }
 
     override suspend fun getById(id: Id): Todo? = notCompleted[id] ?: completed[id]
 
-    override suspend fun delete(todo: Todo) {
+    override suspend fun delete(todo: Todo): Success {
         completed.remove(todo.id)
         notCompleted.remove(todo.id)
+        return true
     }
 
-    override suspend fun markAsCompleted(todo: Todo) {
+    override suspend fun markAsCompleted(todo: Todo): Success {
         notCompleted.remove(todo.id)
         completed[todo.id] = todo
+        return true
     }
 
     override suspend fun isCompleted(id: Id): Boolean = completed.containsKey(id)
 
-    override suspend fun markAsNotCompleted(todo: Todo) {
+    override suspend fun markAsNotCompleted(todo: Todo): Success {
         completed.remove(todo.id)
         notCompleted[todo.id] = todo
+        return true
     }
 
     override suspend fun update(todo: Todo) {
