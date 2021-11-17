a3ccac34f2242d2647b39c48bbb5ee96b8a87191
Mateusz <mateusz.grabarski@gmail.com>
2021-11-17 13:50:12 +0100

    Added abstraction for todo cache and remote data source

--- data/src/main/java/blog/mateuszgrabarski/todo/data/store/cache/TodoCacheDataSource.kt
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/cache/TodoCacheDataSource.kt
@@ -1,4 +1,11 @@
 package blog.mateuszgrabarski.todo.data.store.cache
 
+import blog.mateuszgrabarski.todo.data.model.entities.TodoEntity
+import blog.mateuszgrabarski.todo.domain.models.Id
+
 interface TodoCacheDataSource {
+    suspend fun insert(entity: TodoEntity)
+    suspend fun get(id: Id): TodoEntity?
+    suspend fun update(entity: TodoEntity)
+    suspend fun delete(id: Id)
 }
--- data/src/main/java/blog/mateuszgrabarski/todo/data/store/network/TodoRemoteDataSource.kt
+++ data/src/main/java/blog/mateuszgrabarski/todo/data/store/network/TodoRemoteDataSource.kt
@@ -1,4 +1,10 @@
 package blog.mateuszgrabarski.todo.data.store.network
 
+import blog.mateuszgrabarski.todo.data.model.entities.TodoEntity
+import blog.mateuszgrabarski.todo.domain.models.Id
+
 interface TodoRemoteDataSource {
+    suspend fun insert(entity: TodoEntity)
+    suspend fun delete(id: Id)
+    suspend fun update(entity: TodoEntity)
 }
