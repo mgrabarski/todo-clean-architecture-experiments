705bfc091579d14e555f1fa435cef94aa4b0cc50
Mateusz <mateusz.grabarski@gmail.com>
2021-11-17 15:59:21 +0100

    Added fake cache and remote todo data source

---
+++ data/src/test/java/blog/mateuszgrabarski/todo/data/store/repositories/fakes/FakeTodoCacheDataSource.kt
@@ -0,0 +1,39 @@
+package blog.mateuszgrabarski.todo.data.store.repositories.fakes
+
+import blog.mateuszgrabarski.todo.data.model.entities.TodoEntity
+import blog.mateuszgrabarski.todo.data.store.cache.TodoCacheDataSource
+import blog.mateuszgrabarski.todo.domain.models.Id
+
+class FakeTodoCacheDataSource : TodoCacheDataSource {
+
+    private val notCompleted = mutableMapOf<Id, TodoEntity>()
+    private val completed = mutableMapOf<Id, TodoEntity>()
+
+    override suspend fun insertToNotCompleted(entity: TodoEntity) {
+        notCompleted[entity.id] = entity
+    }
+
+    override suspend fun insertToCompleted(entity: TodoEntity) {
+        completed[entity.id] = entity
+    }
+
+    override suspend fun get(id: Id): TodoEntity? = completed[id] ?: notCompleted[id]
+
+    override suspend fun update(entity: TodoEntity) {
+        if (notCompleted.containsKey(entity.id)) {
+            notCompleted[entity.id] = entity
+        } else {
+            completed[entity.id] = entity
+        }
+    }
+
+    override suspend fun deleteFromCompleted(id: Id) {
+        completed.remove(id)
+    }
+
+    override suspend fun deleteFromNotCompleted(id: Id) {
+        notCompleted.remove(id)
+    }
+
+    override suspend fun isCompleted(id: Id): Boolean = completed.containsKey(id)
+}
